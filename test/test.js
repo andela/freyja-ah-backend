9aab117585856402eac76318975537ec557b7c2a
beejay1293 <mobolajimatti@Mobolajis-MacBook-Pro.local>
2019-05-30 14:29:44 +0100

    fixed eslint issues

--- test/test.js
+++ test/test.js
@@ -1,11 +1,12 @@
 import chai from 'chai';
 import chaiHttp from 'chai-http';
-import { expect } from 'chai';
+
 chai.use(chaiHttp);
+const { expect } = chai;
 
 describe('2+2', () => {
- it('should return 4', (done) => {
-    expect(2+2).to.eql(4);
+  it('should return 4', (done) => {
+    expect(2 + 2).to.eql(4);
     done();
- })
-})
+  });
+});
