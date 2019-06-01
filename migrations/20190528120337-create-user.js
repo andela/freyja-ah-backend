81b4af05345d5f10253e25336d2e2f6613fbf010
Chisom Onwuchekwa <princechekwas@gmail.com>
2019-06-01 14:51:50 +0100

    fix broken tests

--- migrations/20190528120337-create-user.js
+++ migrations/20190528120337-create-user.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /* eslint-disable no-unused-vars */
 
 
@@ -48,53 +47,3 @@ module.exports = {
   }),
   down: (queryInterface, Sequelize) => queryInterface.dropTable('Users')
 };
-=======
-'use strict';
-module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Users', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      firstName: {
-        type: Sequelize.STRING
-      },
-      lastName: {
-        type: Sequelize.STRING
-      },
-      password: {
-        type: Sequelize.STRING
-      },
-      confirmPassword: {
-        type: Sequelize.STRING
-      },
-      email: {
-        type: Sequelize.STRING
-      },
-      age: {
-        type: Sequelize.INTEGER
-      },
-      employed: {
-        type: Sequelize.BOOLEAN
-      },
-      industry: {
-        type: Sequelize.STRING
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Users');
-  }
-};
->>>>>>> 2a1a5ed6c0314b9b9f61ac2b055d243d87883a02
