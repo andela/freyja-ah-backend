1d2a4e29e04b24724a915f46ced58bbbf2bc8545
Chisom Onwuchekwa <princechekwas@gmail.com>
2019-06-20 15:04:35 +0100

    feat(implement get all community messages): write tests for controller method create controller method for this endpoint manually test for functionality
    [delivers #166708096]

--- server/test/messages/communityMessage.js
+++ server/test/messages/communityMessage.js
@@ -12,12 +12,13 @@ import fakeResponse from '../socialLogin.js/fakeResponse';
 
 use(chaihttp);
 
-let userToken = null;
-let userToken2 = null;
+
 const unknownToken = Authenticate.generateToken(2000, 'unknown@mail.com', 'unknown');
 
 
 describe('Post api/community/messages', () => {
+  let userToken = null;
+  let userToken2 = null;
   before((done) => {
     request(server)
       .post('/api/users/login')
@@ -110,6 +111,72 @@ describe('Post api/community/messages', () => {
     const { postMessage } = CommunityMessageController;
     const spy007 = sinon.spy();
     postMessage(fakeUser.userRequest1, fakeResponse, spy007);
+    expect(spy007).to.exist;
+    done();
+  });
+});
+
+describe('Get api/community/messages', () => {
+  let userToken = null;
+  let userToken2 = null;
+  before((done) => {
+    request(server)
+      .post('/api/users/login')
+      .send({ email: 'cruise@mail.com', password: '12345678', })
+      .end((err, res) => {
+        const { token } = res.body;
+        userToken = token;
+      });
+
+    request(server)
+      .post('/api/users/login')
+      .send({ email: 'ted123@mail.com', password: '12345678', })
+      .end((err, res) => {
+        const { token } = res.body;
+        userToken2 = token;
+        done(err);
+      });
+  });
+  it('should get all messages if user is certified or a trainer', (done) => {
+    request(server)
+      .get('/api/community/messages')
+      .set('authorization', userToken)
+      .end((err, res) => {
+        expect(res.status).to.eql(200);
+        expect(res.body).to.have.property('data');
+        expect(res.body.data).to.be.an('array');
+        expect(res.body.message).to.eql('messages returned successfully');
+        done(err);
+      });
+  });
+
+  it('it returns 401 error if user is not a trainer or certified', (done) => {
+    request(server)
+      .get('/api/community/messages')
+      .set('authorization', userToken2)
+      .end((err, res) => {
+        expect(res.status).to.eql(401);
+        expect(res.body.error).to.eql('You are not authorized to view community messages');
+        expect(res.status).to.eql(401);
+        done(err);
+      });
+  });
+
+  it('Should return a 404(Not found) error if a user does not exist', (done) => {
+    request(server)
+      .get('/api/community/messages')
+      .set('authorization', unknownToken)
+      .end((err, res) => {
+        expect(res.status).to.eql(404);
+        expect(res.body).to.have.property('error');
+        expect(res.body.error).to.eql('user not found');
+        done(err);
+      });
+  });
+  it('should spy on next called on catch', (done) => {
+    const { getMessage } = CommunityMessageController;
+    const spy007 = sinon.spy();
+    getMessage(fakeUser.userRequest1, fakeResponse, spy007);
     expect(spy007).to.exist;
     done();
   });
